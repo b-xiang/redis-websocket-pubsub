2c833fc68bd147e35ad665902c580489d5358d66
Tim Dawborn <tim.dawborn@gmail.com>
2015-06-09 21:39:59 +1000

    Let the server accept the opening HTTP handshake.

--- websocket.c
+++ websocket.c
@@ -13,6 +13,7 @@
 #include "base64.h"
 #include "http.h"
 
+// From https://tools.ietf.org/html/rfc6455#section-4.2.2
 static const char *const SEC_WEBSOCKET_KEY_GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 
 
@@ -42,7 +43,7 @@ http_response_write_no_body(const unsigned int status_code, const char *const re
   }
 
   nbytes = write(fd, "\r\n", 2);
-  if (nbytes == -1) {
+  if (nbytes != 2) {
     return STATUS_BAD;
   }
 
@@ -58,6 +59,7 @@ websocket_write_http_response(const struct http_request *const req, const int fd
   const struct http_request_header *header = NULL;
   unsigned char *sha1_input_buffer = NULL;
   unsigned char sha1_output_buffer[SHA_DIGEST_LENGTH];
+  ssize_t nbytes;
 
   if (req == NULL) {
     return STATUS_EINVAL;
@@ -87,6 +89,10 @@ websocket_write_http_response(const struct http_request *const req, const int fd
   header = http_request_find_header(req, "SEC-WEBSOCKET-VERSION");
   if (header == NULL || strcmp("13", header->value) != 0) {
     http_response_write_no_body(400, "Bad Request", fd);
+    nbytes = write(fd, "Sec-WebSocket-Version: 13\r\n", 27);
+    if (nbytes != 27) {
+      return STATUS_BAD;
+    }
     return STATUS_OK;
   }
 
@@ -94,6 +100,13 @@ websocket_write_http_response(const struct http_request *const req, const int fd
   header = http_request_find_header(req, "SEC-WEBSOCKET-KEY");
   if (header == NULL) {
     http_response_write_no_body(400, "Bad Request", fd);
+    return STATUS_OK;
+  }
+
+  // Look for the `Origin` HTTP header in the request.
+  header = http_request_find_header(req, "ORIGIN");
+  if (header == NULL) {
+    http_response_write_no_body(403, "Forbidden", fd);
     return STATUS_OK;
   }
 
@@ -109,12 +122,6 @@ websocket_write_http_response(const struct http_request *const req, const int fd
   SHA1(sha1_input_buffer, key_nbytes + guid_nbytes, sha1_output_buffer);
   free(sha1_input_buffer);
 
-  fprintf(stdout, "sha1 output buffer:");
-  for (size_t i = 0; i != SHA_DIGEST_LENGTH; ++i) {
-    fprintf(stdout, " 0x%02x", sha1_output_buffer[i]);
-  }
-  fprintf(stdout, "\n");
-
   // Convert the SHA1 hash bytes into its base64 representation.
   base64_status = base64_init(&sha1_base64_buffer);
   if (base64_status != STATUS_OK) {
@@ -126,14 +133,38 @@ websocket_write_http_response(const struct http_request *const req, const int fd
     return base64_status;
   }
 
-  fprintf(stdout, "base64(sha1 output buffer): '");
-  fwrite(sha1_base64_buffer.data, 1, sha1_base64_buffer.used, stdout);
-  fprintf(stdout, "'\n");
-
-  base64_status = base64_destroy(&sha1_base64_buffer);
-  if (base64_status != STATUS_OK) {
-    return base64_status;
+  // Send the server's opening handshake to accept the incomming connection.
+  // https://tools.ietf.org/html/rfc6455#section-4.2.2
+  nbytes = write(fd, "HTTP/1.1 101 Switching Protocols\r\n", 34);
+  if (nbytes != 34) {
+    return STATUS_BAD;
   }
+  nbytes = write(fd, "Upgrade: websocket\r\n", 20);
+  if (nbytes != 20) {
+    return STATUS_BAD;
+  }
+  nbytes = write(fd, "Connection: Upgrade\r\n", 21);
+  if (nbytes != 21) {
+    return STATUS_BAD;
+  }
+  nbytes = write(fd, "Sec-WebSocket-Accept: ", 22);
+  if (nbytes != 22) {
+    return STATUS_BAD;
+  }
+  nbytes = write(fd, sha1_base64_buffer.data, sha1_base64_buffer.used);
+  if (nbytes != (ssize_t)sha1_base64_buffer.used) {
+    return STATUS_BAD;
+  }
+  nbytes = write(fd, "\r\n", 2);
+  if (nbytes != 2) {
+    return STATUS_BAD;
+  }
+  nbytes = write(fd, "\r\n", 2);
+  if (nbytes != 2) {
+    return STATUS_BAD;
+  }
+
+  base64_destroy(&sha1_base64_buffer);
 
   return STATUS_OK;
 }
